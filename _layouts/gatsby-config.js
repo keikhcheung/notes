e3a58273b1f6c4fa02c8aa64ea57d6e57d5df2a4
kei <kei.khcheung@gmail.com>
2021-02-07 21:11:20 +0100

    twitter card

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   pathPrefix: ``,
   siteMetadata: {
     title: `kei’s notes`,
+    siteUrl: "https://notes.keikhcheung.com",
   },
   plugins: [
     {
@@ -17,6 +18,21 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-remark-twitter-cards`,
+      options: {
+        title: 'notes', // website title
+        separator: '|', // default
+        author: 'keikeckeung',
+        background: require.resolve('./src/twitter-card-notes.png'), // path to 1200x630px file or hex code, defaults to black (#000000)
+        fontColor: '#000000', // defaults to white (#ffffff)
+        titleFontSize: 96, // default
+        subtitleFontSize: 60, // default
+        fontStyle: 'monospace', // default
+        fontFile: require.resolve('./src/SpaceGrotesk-Regular.ttf'), // will override fontStyle - path to custom TTF font
+        useFrontmatterSlug: false // default, if true it will use the slug defined in the post frontmatter
+      }
+    }
     `gatsby-plugin-favicon`
   ],
 };
